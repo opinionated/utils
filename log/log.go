212958137cf6ef5924d10fdbfc906829b1159b05
ConnorFoody <connorfoody>
2015-11-23 13:34:39 -0500

    updated logger

--- log/log.go
+++ log/log.go
@@ -1,9 +1,14 @@
 package log
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -12,44 +17,73 @@ var (
 	err     *log.Logger
 )
 
-// Info writes an info message to the log.
-func Info(s ...interface{}) {
-	info.Println(s...)
+func getCallerString() string {
+	var buffer bytes.Buffer
+
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		warning.Println("getCallerString could not get its caller!")
+		return ""
+	}
+
+	// get the index
+	opinionatedIndex := strings.LastIndex(file, "opinionated")
+	if opinionatedIndex == -1 {
+		warning.Println("getCallerString not called from an opinionated repo")
+		return ""
+	}
+
+	buffer.WriteString(file[opinionatedIndex+11+1:])
+	buffer.WriteRune(':')
+	buffer.WriteString(strconv.Itoa(line))
+	buffer.WriteRune(' ')
+	return buffer.String()
 }
 
-// Infof writes a formatted info message to the log.
+// Info writes an info message to the log.
+func Info(s ...interface{}) {
+	outer := fmt.Sprintln(s...)
+	info.Print(getCallerString(), outer)
+}
+
+// Infof writes a formatted info message to the log. Ends with new line.
 func Infof(format string, args ...interface{}) {
-	info.Printf(format, args...)
+	outer := fmt.Sprintf(format, args...)
+	info.Print(getCallerString(), outer)
 }
 
 // Warn writes a warning message to the warning log.
 func Warn(s ...interface{}) {
-	warning.Println(s...)
+	outer := fmt.Sprintln(s...)
+	warning.Print(getCallerString(), outer)
 }
 
-// Warnf write a formatted warning message to the warning log.
+// Warnf write a formatted warning message to the warning log. Ends with new line.
 func Warnf(format string, args ...interface{}) {
-	warning.Printf(format, args...)
+	outer := fmt.Sprintf(format, args...)
+	warning.Print(getCallerString(), outer)
 }
 
 // Error write a message to the log. Does not fatal or panic.
 func Error(s ...interface{}) {
-	err.Println(s...)
+	outer := fmt.Sprintln(s...)
+	err.Print(getCallerString(), outer)
 }
 
-// Fatal writes a message to the error log then calls sys.exit(1)
+// Fatal writes a message to the error log then calls sys.exit(1).
 func Fatal(s ...interface{}) {
-	err.Fatal(s...)
+	outer := fmt.Sprintln(s...)
+	err.Fatal(getCallerString(), outer)
 }
 
-// Initialize logs to write to standard output
+// Initialize logs to write to standard output.
 func InitStd() {
 	Init(os.Stdout, os.Stdout, os.Stdout)
 }
 
 // Init initializes the logs.
 func Init(i io.Writer, w io.Writer, e io.Writer) {
-	info = log.New(i, "INFO: ", log.Ltime|log.Lshortfile)
-	warning = log.New(i, "INFO: ", log.Ltime|log.Lshortfile)
-	err = log.New(i, "INFO: ", log.Ltime|log.Lshortfile)
+	info = log.New(i, "INFO: ", log.Ltime)
+	warning = log.New(i, "INFO: ", log.Ltime)
+	err = log.New(i, "INFO: ", log.Ltime)
 }
--- log/log_test.go
+++ log/log_test.go
@@ -1,10 +1,17 @@
 package log_test
 
 import (
+	"flag"
 	"github.com/opinionated/utils/log"
 	"os"
 	"testing"
 )
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	log.InitStd()
+	os.Exit(m.Run())
+}
 
 func TestWarning(t *testing.T) {
 	log.Init(os.Stdout, os.Stdout, os.Stdout)
@@ -17,4 +24,11 @@ func TestWarning(t *testing.T) {
 func TestInitStd(t *testing.T) {
 	log.InitStd()
 	log.Info("hello world", "from", "info")
+	log.Infof("one is: %d", 1)
+	log.Info("two")
+	log.Infof("three is: %d", 3)
+}
+
+func TestLine(t *testing.T) {
+	log.Info("test")
 }
